2c011a1e658adab2920df77c4b68c7eba6a8d3c8
Granottier Redemptor <omedaan11@gmail.com>
2023-09-19 20:07:16 +0530

    Fix name-stb time-font-size property-aggregator

--- video_strip_preview.sh
+++ video_strip_preview.sh
@@ -4,10 +4,11 @@ ARGV=("$@")
 ARGC=$#
 
 INPUT="$1"
-OUTPUT="$(echo "$(basename "$1")"|sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
+OUTPUT="Preview_$(echo "$(basename "$1")"|sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
 echo "output = $OUTPUT"
 TITLE="$(basename "$1")"
 echo "title = $TITLE"
+TITLE_SAN=$(echo $TITLE|sed -E 's~[^a-z^A-Z^0-9^\.^ ^\-^_]~~g')
 SIZE=360
 BORDER=10
 PADDING=10
@@ -17,28 +18,29 @@ TOTAL_PREV=$((COLUMN * ROW))
 echo "Preview image count = $TOTAL_PREV"
 Tot_Frames="$(ffmpeg -i "$1" -map 0:v:0 -c:v copy -f null /dev/null 2>&1 | grep -oP 'frame=[\s]*\K\d+' | tail -n1)"
 echo "total no. of frames = $Tot_Frames"
-Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "$INPUT"|grep -zP "(?:<\!\[PROGRAM\]\n)(?<=\[STREAM\]\nduration=)[\. 0-9]+\n")"
+Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_long_name "$INPUT"|grep -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\ncodec_long_name=\K[^\n]+\n'|tr -d '\0')"
 echo "Input File Encoding = $Encoding"
-Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=nokey=1:noprint_wrappers=1 "$INPUT")"
+Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "$INPUT"|grep -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nduration=\K[\d \.]+\n'|tr -d '\0')"
 Duration=${Duration%.*}
 echo "Video Duration = $Duration"
 Duration_F="$(($((${Duration%.*} / 3600)) % 24))\:$(($((${Duration%.*} / 60)) % 60))\:$((${Duration%.*} % 60))"
 echo "Formatted Video Duration = $Duration_F"
 #FRMS_TIME=$(ffprobe -v error -select_streams v:0 -show_entries program_stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")
-FPS=$(($(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")))
+FPS=$(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate "$INPUT"|grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\navg_frame_rate=\K[\d \. \/]+\n'|tr -d '\0')
+FPS=$(($FPS))
 echo "Video FPS = $FPS"
 SKIP=$((Tot_Frames / TOTAL_PREV))
 echo "Frame Gap = $SKIP"
-W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nokey=1:noprint_wrappers=1 "$1")"
+W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width "$INPUT"|grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nwidth=\K[\d]+\n'|tr -d '\0')"
 echo "width of video = $W_Frame"
-H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nokey=1:noprint_wrappers=1 "$1")"
+H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height "$INPUT"|grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nheight=\K[\d]+\n'|tr -d '\0')"
 echo "height of video = $H_Frame"
 
 FONT="/usr/share/fonts/TTF/OpenSans-Regular.ttf"
 TIME="%{eif\:t/3600\:d\:2}\:%{eif\:mod(t/60\,60)\:d\:2}\:%{eif\:mod(t\,60)\:d\:2}\.%{eif\:100*mod(t\,1)\:d\:2}"
 
 #TIME_FONT_SIZE=$((W_Frame/1280 * 48))
-TIME_FONT_SIZE=$(($(( W_Frame > H_Frame ? W_Frame : H_Frame ))/640 * 48))
+TIME_FONT_SIZE=$(($(($(( W_Frame > H_Frame ? W_Frame : H_Frame ))/$(( W_Frame < H_Frame ? W_Frame : H_Frame )) * W_Frame)) / 10))
 echo "Frame time font size = $TIME_FONT_SIZE"
 FONT_PROP=":fontcolor=white:fontsize=$TIME_FONT_SIZE"
 echo "Font properties = $FONT_PROP"
@@ -67,9 +69,9 @@ ffmpeg -i "$INPUT" -y -vframes 1 -q:v 2 -vf "select=not(mod(n\\,$SKIP)),\
                                                     scale=$SIZE:-1,\
                                                     tile=${COLUMN}x${ROW}:padding=$PADDING:margin=$BORDER:color=Black,\
                                                     pad=width=2+iw:height=2+ih+$TITLE_SPACE:x=1:y=1+$TITLE_SPACE:color=black,\
-                                                    drawtext=fontfile="$FONT":text='Title - $TITLE'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20,\
+                                                    drawtext=fontfile="$FONT":text='Title - ${TITLE_SAN}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20,\
                                                     drawtext=fontfile="$FONT":text='Dimentions - ${H_Frame} x ${W_Frame} | FPS - ${FPS}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*1.5,\
                                                     drawtext=fontfile="$FONT":text='Encoding - ${Encoding}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*3,\
                                                     drawtext=fontfile="$FONT":text='Duration - ${Duration_F}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*4.5"\
-                                                    "$OUTPUT"
+                                                    "$OUTPUT" 2>&1
 
