858f40c0ea59a6b56267aa03b5a6a8ef54e0ca09
Granottier Redemptor <omedaan11@gmail.com>
2023-09-22 20:06:44 +0530

    trying to fix the whole reverting debocle

--- video_strip_preview.sh
+++ video_strip_preview.sh
@@ -4,37 +4,25 @@ ARGV=("$@")
 ARGC=$#
 
 INPUT="$1"
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 OUTPUT="Preview_$(echo   "$(basename "$1")"|\
 							sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
-=======
-OUTPUT="$(echo "$(basename "$1")"|sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
-=======
-OUTPUT="$(echo "$(basename "$1")"|sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 echo "output = $OUTPUT"
 
 TITLE="$(basename "$1")"
 echo "title = $TITLE"
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 TITLE_SAN=$(echo $TITLE|\
 			sed -E 's~[^a-z^A-Z^0-9^\.^ ^\-^_]~~g')
 LENGTH=1920
-=======
-=======
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 SIZE=360
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 BORDER=10
 PADDING=10
 COLUMN=5
 ROW=3
-SIZE=372
-#SIZE=$(($(($((LENGTH-BORDER*2)) - $(($((COLUMN - 1)) * PADDING)))) / COLUMN))
+#SIZE=372
+SIZE=$(($(($((LENGTH-BORDER*2)) - $(($((COLUMN - 1)) * PADDING)))) / COLUMN))
 echo "Reverse Calculated Size = $SIZE"
 
 TOTAL_PREV=$((COLUMN * ROW))
@@ -43,8 +31,7 @@ Tot_Frames="$(ffmpeg -i "$1" -map 0:v:0 -c:v copy -f null /dev/null 2>&1 |\
 				grep -oP 'frame=[\s]*\K\d+' |\
 				tail -n1)"
 echo "total no. of frames = $Tot_Frames"
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_long_name "$INPUT"|\
 			grep -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\ncodec_long_name=\K[^\n]+\n'|\
@@ -54,16 +41,8 @@ echo "Input File Encoding = $Encoding"
 Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "$INPUT"|\
 			grep -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nduration=\K[\d \.]+\n'|\
 			tr -d '\0')"
-=======
-Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "$INPUT"|grep -zP "(?:<\!\[PROGRAM\]\n)(?<=\[STREAM\]\nduration=)[\. 0-9]+\n")"
-echo "Input File Encoding = $Encoding"
-Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=nokey=1:noprint_wrappers=1 "$INPUT")"
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
-=======
-Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "$INPUT"|grep -zP "(?:<\!\[PROGRAM\]\n)(?<=\[STREAM\]\nduration=)[\. 0-9]+\n")"
-echo "Input File Encoding = $Encoding"
-Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=nokey=1:noprint_wrappers=1 "$INPUT")"
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
+
+
 Duration=${Duration%.*}
 echo "Video Duration = $Duration"
 
@@ -71,25 +50,17 @@ Duration_F="$(($((${Duration%.*} / 3600)) % 24))\:$(($((${Duration%.*} / 60)) % 
 echo "Formatted Video Duration = $Duration_F"
 
 #FRMS_TIME=$(ffprobe -v error -select_streams v:0 -show_entries program_stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 FPS=$(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate "$INPUT"|\
 		grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\navg_frame_rate=\K[\d \. \/]+\n'|\
 		tr -d '\0')
 FPS=$(($FPS))
-=======
-FPS=$(($(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")))
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
-=======
-FPS=$(($(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")))
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
+
 echo "Video FPS = $FPS"
 
 SKIP=$((Tot_Frames / TOTAL_PREV))
 echo "Frame Gap = $SKIP"
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width "$INPUT"|\
 			grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nwidth=\K[\d]+\n'|\
@@ -99,16 +70,6 @@ echo "width of video = $W_Frame"
 H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height "$INPUT"|\
 			grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nheight=\K[\d]+\n'|\
 			tr -d '\0')"
-=======
-W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nokey=1:noprint_wrappers=1 "$1")"
-echo "width of video = $W_Frame"
-H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nokey=1:noprint_wrappers=1 "$1")"
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
-=======
-W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nokey=1:noprint_wrappers=1 "$1")"
-echo "width of video = $W_Frame"
-H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nokey=1:noprint_wrappers=1 "$1")"
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 echo "height of video = $H_Frame"
 
 FONT="/usr/share/fonts/TTF/OpenSans-Regular.ttf"
@@ -142,7 +103,7 @@ do
 echo "Final Echo"
 done
 #exit 0
-<<<<<<< HEAD
+
 ffmpeg -i "$INPUT" -y -vframes 1 -q:v 2 -vf \
 	"select=not(mod(n\\,$SKIP)),\
 	drawtext=fontfile="$FONT":text='$TIME'$FONT_PROP$BOX_PROP:x=($W_Frame-text_w)-10:y=($H_Frame-text_h)-10,\
@@ -154,19 +115,3 @@ ffmpeg -i "$INPUT" -y -vframes 1 -q:v 2 -vf \
 	drawtext=fontfile="$FONT":text='Encoding - ${Encoding}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*3,\
 	drawtext=fontfile="$FONT":text='Duration - ${Duration_F}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*4.5"\
 	"$OUTPUT" 2>&1
-=======
-ffmpeg -i "$INPUT" -y -vframes 1 -q:v 2 -vf "select=not(mod(n\\,$SKIP)),\
-                                                    drawtext=fontfile="$FONT":text='$TIME'$FONT_PROP$BOX_PROP:x=($W_Frame-text_w)-10:y=($H_Frame-text_h)-10,\
-                                                    scale=$SIZE:-1,\
-                                                    tile=${COLUMN}x${ROW}:padding=$PADDING:margin=$BORDER:color=Black,\
-                                                    pad=width=2+iw:height=2+ih+$TITLE_SPACE:x=1:y=1+$TITLE_SPACE:color=black,\
-                                                    drawtext=fontfile="$FONT":text='Title - $TITLE'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20,\
-                                                    drawtext=fontfile="$FONT":text='Dimentions - ${H_Frame} x ${W_Frame} | FPS - ${FPS}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*1.5,\
-                                                    drawtext=fontfile="$FONT":text='Encoding - ${Encoding}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*3,\
-                                                    drawtext=fontfile="$FONT":text='Duration - ${Duration_F}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*4.5"\
-                                                    "$OUTPUT"
-<<<<<<< HEAD
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
-=======
->>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
-
