75f6baafd69cf70a7bcb25009f4ab0057cf16a69
Granottier Redemptor <omedaan11@gmail.com>
2023-09-22 19:55:05 +0530

    Revert "Fix name-stb time-font-size property-aggregator"
    This reverts commit 2c011a1e658adab2920df77c4b68c7eba6a8d3c8.

--- video_strip_preview.sh
+++ video_strip_preview.sh
@@ -5,8 +5,12 @@ ARGC=$#
 
 INPUT="$1"
 <<<<<<< HEAD
+<<<<<<< HEAD
 OUTPUT="Preview_$(echo   "$(basename "$1")"|\
 							sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
+=======
+OUTPUT="$(echo "$(basename "$1")"|sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
+>>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 =======
 OUTPUT="$(echo "$(basename "$1")"|sed -E "s~(.*\.)[^\.]*$~\1jpg~g")"
 >>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
@@ -15,11 +19,14 @@ echo "output = $OUTPUT"
 TITLE="$(basename "$1")"
 echo "title = $TITLE"
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 TITLE_SAN=$(echo $TITLE|\
 			sed -E 's~[^a-z^A-Z^0-9^\.^ ^\-^_]~~g')
 LENGTH=1920
 =======
+=======
+>>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 SIZE=360
 >>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 BORDER=10
@@ -37,6 +44,7 @@ Tot_Frames="$(ffmpeg -i "$1" -map 0:v:0 -c:v copy -f null /dev/null 2>&1 |\
 				tail -n1)"
 echo "total no. of frames = $Tot_Frames"
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_long_name "$INPUT"|\
 			grep -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\ncodec_long_name=\K[^\n]+\n'|\
@@ -51,6 +59,11 @@ Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "
 echo "Input File Encoding = $Encoding"
 Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=nokey=1:noprint_wrappers=1 "$INPUT")"
 >>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
+=======
+Encoding="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration "$INPUT"|grep -zP "(?:<\!\[PROGRAM\]\n)(?<=\[STREAM\]\nduration=)[\. 0-9]+\n")"
+echo "Input File Encoding = $Encoding"
+Duration="$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=nokey=1:noprint_wrappers=1 "$INPUT")"
+>>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 Duration=${Duration%.*}
 echo "Video Duration = $Duration"
 
@@ -58,6 +71,7 @@ Duration_F="$(($((${Duration%.*} / 3600)) % 24))\:$(($((${Duration%.*} / 60)) % 
 echo "Formatted Video Duration = $Duration_F"
 
 #FRMS_TIME=$(ffprobe -v error -select_streams v:0 -show_entries program_stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 FPS=$(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate "$INPUT"|\
@@ -67,10 +81,14 @@ FPS=$(($FPS))
 =======
 FPS=$(($(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")))
 >>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
+=======
+FPS=$(($(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=nokey=1:noprint_wrappers=1 "$INPUT")))
+>>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 echo "Video FPS = $FPS"
 
 SKIP=$((Tot_Frames / TOTAL_PREV))
 echo "Frame Gap = $SKIP"
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width "$INPUT"|\
@@ -81,6 +99,11 @@ echo "width of video = $W_Frame"
 H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height "$INPUT"|\
 			grep  -ozP '(?<![\[]PROGRAM[\]]\n)[\[]STREAM[\]]\nheight=\K[\d]+\n'|\
 			tr -d '\0')"
+=======
+W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nokey=1:noprint_wrappers=1 "$1")"
+echo "width of video = $W_Frame"
+H_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nokey=1:noprint_wrappers=1 "$1")"
+>>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 =======
 W_Frame="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nokey=1:noprint_wrappers=1 "$1")"
 echo "width of video = $W_Frame"
@@ -142,5 +165,8 @@ ffmpeg -i "$INPUT" -y -vframes 1 -q:v 2 -vf "select=not(mod(n\\,$SKIP)),\
                                                     drawtext=fontfile="$FONT":text='Encoding - ${Encoding}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*3,\
                                                     drawtext=fontfile="$FONT":text='Duration - ${Duration_F}'$TIT_FONT_PROP$TIT_BOX_PROP:x=20:y=20+$TIT_FONT_SIZE*4.5"\
                                                     "$OUTPUT"
+<<<<<<< HEAD
+>>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
+=======
 >>>>>>> parent of 2c011a1 (Fix name-stb time-font-size property-aggregator)
 
